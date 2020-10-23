f7d710b1d86752e8306ac29e3d5f7e2f704bc4ad
wesley.wu <wesley.wu@expel.io>
2020-10-23 08:41:41 -0400

    adhoc: add docs favicon

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,7 @@ html_theme_options = {
 }
 
 html_logo = '_static/img/expel-logo.png'
+html_favicon = '_static/img/favicon.ico'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
