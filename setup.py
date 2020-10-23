932a767e4b1b724503cbce3e1dc6155cce3660c7
wesley.wu <wesley.wu@expel.io>
2020-10-23 15:31:10 -0400

    adhoc: add readme to setup long_description

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup
 
 import versioneer
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='pyexclient',
     description='Expel Workbench Client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Expel Inc.',
     url='https://github.com/expel-io/pyexclient',
     license='BSD',
