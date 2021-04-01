e8e5796d6d765d4d88b75f010245240981042b03
Abhishek <abhishek@query.ai>
2021-04-01 11:27:28 +0530

    Skip recreate session if 'retries' is set to False or 0

--- pyexclient/workbench.py
+++ pyexclient/workbench.py
@@ -4917,18 +4917,23 @@ class WorkbenchCoreClient:
                     data=data,
                     **request_kwargs
                 )
-            except ConnectionError:
-                # if connection was fatally closed, create a new session and try again
-                logger.warning("got connection error, recreating session...")
-                time.sleep(5)
-                self.make_session()
-                resp = self.session.request(
-                    method=method,
-                    url=url,
-                    headers=headers,
-                    data=data,
-                    **request_kwargs
-                )
+            except ConnectionError as e:
+                if self.retries > 0 or ('retries' in request_kwargs and not request_kwargs['retries']):
+                    # if connection was fatally closed, create a new session and try again
+                    logger.warning("got connection error, recreating session...")
+                    time.sleep(5)
+                    self.make_session()
+                    resp = self.session.request(
+                        method=method,
+                        url=url,
+                        headers=headers,
+                        data=data,
+                        **request_kwargs
+                    )
+                else:
+                    # skip recreating a new session if retries is False or 0
+                    logger.warning("got connection error, skip recreating session...")
+                    raise Exception(e)
 
         if self.debug and do_print:
             logger.debug(pprint.pformat(resp.json()))
@@ -4983,9 +4988,9 @@ class WorkbenchClient(WorkbenchCoreClient):
     :rtype: WorkbenchClient
     '''
 
-    def __init__(self, base_url, username=None, password=None, mfa_code=None, token=None, prompt_on_delete=True):
+    def __init__(self, base_url, username=None, password=None, mfa_code=None, token=None, prompt_on_delete=True, retries=0):
         super().__init__(base_url, username=username, password=password,
-                         mfa_code=mfa_code, token=token, prompt_on_delete=prompt_on_delete)
+                         mfa_code=mfa_code, token=token, prompt_on_delete=prompt_on_delete, retries=retries)
 
     def create_manual_inv_action(self, title: str, reason: str, instructions: str, investigation_id: str = None, expel_alert_id: str = None, security_device_id: str = None, action_type: str = 'MANUAL'):
         '''
