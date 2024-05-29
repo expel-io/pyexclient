a208da6654b84d6f58e3ef4e6a80b94f536b3c1b
Daren McCulley <daren.mcculley@expel.io>
2024-05-29 11:14:43 -0400

    fix conf

--- docs/conf.py
+++ docs/conf.py
@@ -102,18 +102,10 @@ html_favicon = '_static/img/favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 #html_style = 'css/expel.css'
-html_context = {
-    'css_files': [
-        # for some reason theme.css isn't shipping 
-        # copied the css from the working 1.0.0 build and provided it as an override for now
-        # we need to be able to update the content of these docs without worrying about CSS
-        # or keeping up with changes to the latest sphinx themes
-        '_static/css/theme.css',
-        '_static/css/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-}
+
+html_css_files = ["_static/theme_overrides.css"]  # override wide tables in RTD theme
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
